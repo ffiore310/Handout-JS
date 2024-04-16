b6207751ca72230f7b8c4e78aeb6296b3e2defbe
Fernando Fiore <fernandoof@al.insper.edu.br>
2024-04-16 08:25:08 -0300

    soma feito

--- client.js
+++ client.js
@@ -35,13 +35,39 @@ async function getExercises(token) {
   
 }
 
+async function postSoma(soma, token) {
+    const url = 'https://tecweb-js.insper-comp.com.br/exercicio/soma';
+    const body = { resposta: soma };
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    };
+  
+    const response = await axios.post(url, body, config);
+    return response.data; 
+}
+
 async function main() {
     const token = await getToken('fernandoof');
     console.log('Token obtido:', token);
     if (token) {
+      //Puxa lista de exercicios
       const exercises = await getExercises(token);
       console.log('Lista de exercícios:', exercises);
+
+      //Faz o exercicio de soma
+      add = exercises.soma.entrada;
+      console.log('Valores para soma:', add);
+      const r_soma = await postSoma(soma(add.a, add.b), token);
+      console.log('Resultado soma de valores:', r_soma);
+      }
   }
+
+function soma(a, b) {
+    return a + b;
 }
 
 main();

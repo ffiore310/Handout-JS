ceaac0e1e0144a797cf9eb71070718a780c1f229
Fernando Fiore <fernandoof@al.insper.edu.br>
2024-04-18 11:43:10 -0300

    nota 10

--- client.js
+++ client.js
@@ -58,89 +58,95 @@ async function main() {
       const exercises = await getExercises(token);
       console.log('Lista de exercícios:', exercises);
 
-    //   //Faz o exercicio de soma
-    //   add = exercises.soma.entrada;
-    //   //console.log('Valores para soma:', add);
-    //   const r_soma = await postResposta('soma',soma(add.a, add.b), token);
-    //   console.log('Resultado soma de valores:', r_soma);
+      //Faz o exercicio de soma
+      add = exercises.soma.entrada;
+      //console.log('Valores para soma:', add);
+      const r_soma = await postResposta('soma',soma(add.a, add.b), token);
+      console.log('Resultado soma de valores:', r_soma);
 
-    //   //Faz o exercicio de contagem de string
-    //   string = exercises["tamanho-string"].entrada.string;
-    //   const r_string = await postResposta('tamanho-string',conta_string(string), token);
-    //   console.log('Resultado da contagem da string:', r_string);
+      //Faz o exercicio de contagem de string
+      string = exercises["tamanho-string"].entrada.string;
+      const r_string = await postResposta('tamanho-string',conta_string(string), token);
+      console.log('Resultado da contagem da string:', r_string);
 
-    //   //Faz o exercicio de nome do usuario
-    //   nome = exercises["nome-do-usuario"].entrada.email;
-    //   const r_nome = await postResposta('nome-do-usuario',nome_usuario(nome), token);
-    //   console.log('Resultado do nome do usuario:', r_nome);
+      //Faz o exercicio de nome do usuario
+      nome = exercises["nome-do-usuario"].entrada.email;
+      const r_nome = await postResposta('nome-do-usuario',nome_usuario(nome), token);
+      console.log('Resultado do nome do usuario:', r_nome);
 
-    //   //Jaca Wars
-    //   parametros = exercises["jaca-wars"].entrada;
-    //   vel = parametros.v;
-    //   theta = parametros.theta;
-    //   const r_jaca = await postResposta('jaca-wars',distancia(vel,theta), token);
-    //   console.log('Resultado jaca-wars:', r_jaca);
+      //Jaca Wars
+      parametros = exercises["jaca-wars"].entrada;
+      vel = parametros.v;
+      theta = parametros.theta;
+      const r_jaca = await postResposta('jaca-wars',distancia(vel,theta), token);
+      console.log('Resultado jaca-wars:', r_jaca);
 
-    //   //Ano bissexto
-    //   ano = exercises["ano-bissexto"].entrada.ano;
-    //   const r_bissexto = await postResposta('ano-bissexto',bissexto(ano), token);
-    //   console.log('Resultado ano bissexto:', r_bissexto);
+      //Ano bissexto
+      ano = exercises["ano-bissexto"].entrada.ano;
+      const r_bissexto = await postResposta('ano-bissexto',bissexto(ano), token);
+      console.log('Resultado ano bissexto:', r_bissexto);
 
-    //   //Volume da pizza
-    //   parametros = exercises["volume-da-pizza"].entrada;
-    //   z = parametros.z;
-    //   a = parametros.a;
-    //   const r_volume = await postResposta('volume-da-pizza',volume(z,a), token);
-    //   console.log('Resultado volume da pizza:', r_volume);
+      //Volume da pizza
+      parametros = exercises["volume-da-pizza"].entrada;
+      z = parametros.z;
+      a = parametros.a;
+      const r_volume = await postResposta('volume-da-pizza',volume(z,a), token);
+      console.log('Resultado volume da pizza:', r_volume);
 
-    //   //MRU
-    //   parametros = exercises["mru"].entrada;
-    //   s0 = parametros.s0;
-    //   v = parametros.v;
-    //   t = parametros.t;
-    //   const r_mru = await postResposta('mru',mru(s0,v,t), token);
-    //   console.log('Resultado MRU:', r_mru);
+      //MRU
+      parametros = exercises["mru"].entrada;
+      s0 = parametros.s0;
+      v = parametros.v;
+      t = parametros.t;
+      const r_mru = await postResposta('mru',mru(s0,v,t), token);
+      console.log('Resultado MRU:', r_mru);
 
-    //   //Inverte string
-    //   s = exercises["inverte-string"].entrada.string;
-    //   const r_inverte = await postResposta('inverte-string',inverte(s), token);
-    //   console.log('Resultado inverte string:', r_inverte);
+      //Inverte string
+      s = exercises["inverte-string"].entrada.string;
+      const r_inverte = await postResposta('inverte-string',inverte(s), token);
+      console.log('Resultado inverte string:', r_inverte);
 
-    //   //Soma valores
-    //   d = exercises["soma-valores"].entrada.objeto;
-    //   const r_soma_valores = await postResposta('soma-valores',soma_valores(d), token);
-    //   console.log('Resultado soma valores:', r_soma_valores);
+      //Soma valores
+      d = exercises["soma-valores"].entrada.objeto;
+      const r_soma_valores = await postResposta('soma-valores',soma_valores(d), token);
+      console.log('Resultado soma valores:', r_soma_valores);
 
-    //   //n-esimo-primo
-    //   n = exercises["n-esimo-primo"].entrada.n;
-    //   const r_primo = await postResposta('n-esimo-primo',n_primos(n), token);
-    //   console.log('Resultado n-esimo primo:', r_primo);
+      //n-esimo-primo
+      n = exercises["n-esimo-primo"].entrada.n;
+      const r_primo = await postResposta('n-esimo-primo',n_primos(n), token);
+      console.log('Resultado n-esimo primo:', r_primo);
 
-    //   //Prefixo comum
-    //     array_p = exercises["maior-prefixo-comum"].entrada.strings;
-    //     const r_prefixo = await postResposta('maior-prefixo-comum',maior_prefixo(array_p), token);
-    //     console.log('Resultado maior prefixo comum:', r_prefixo);
+      //Prefixo comum
+        array_p = exercises["maior-prefixo-comum"].entrada.strings;
+        const r_prefixo = await postResposta('maior-prefixo-comum',maior_prefixo(array_p), token);
+        console.log('Resultado maior prefixo comum:', r_prefixo);
 
-    //     //Soma segundos
-    //     array_s = exercises['soma-segundo-maior-e-menor-numeros'].entrada.numeros;
-    //     const r_soma_segundos = await postResposta('soma-segundo-maior-e-menor-numeros',soma_segundos(array_s), token);
-    //     console.log('Resultado soma segundos:', r_soma_segundos);
+        //Soma segundos
+        array_s = exercises['soma-segundo-maior-e-menor-numeros'].entrada.numeros;
+        const r_soma_segundos = await postResposta('soma-segundo-maior-e-menor-numeros',soma_segundos(array_s), token);
+        console.log('Resultado soma segundos:', r_soma_segundos);
 
-    //     //Conta palindromos
-    //     array_pal = exercises['conta-palindromos'].entrada.palavras;
-    //     const r_palindromos = await postResposta('conta-palindromos',conta_palindromos(array_pal), token);
-    //     console.log('Resultado conta palindromos:', r_palindromos);
+        //Conta palindromos
+        array_pal = exercises['conta-palindromos'].entrada.palavras;
+        const r_palindromos = await postResposta('conta-palindromos',conta_palindromos(array_pal), token);
+        console.log('Resultado conta palindromos:', r_palindromos);
 
-    //     //Soma strings
-    //     array_str = exercises['soma-de-strings-de-ints'].entrada.strings;
-    //     const r_soma_strings = await postResposta('soma-de-strings-de-ints',soma_strings(array_str), token);
-    //     console.log('Resultado soma strings:', r_soma_strings);
+        //Soma strings
+        array_str = exercises['soma-de-strings-de-ints'].entrada.strings;
+        const r_soma_strings = await postResposta('soma-de-strings-de-ints',soma_strings(array_str), token);
+        console.log('Resultado soma strings:', r_soma_strings);
 
         //Soma com requisicoes
         array_req = exercises['soma-com-requisicoes'].entrada.endpoints;
         let resultado = await soma_com_requisicoes(array_req, token);
         const r_soma_req = await postResposta('soma-com-requisicoes',resultado, token);
         console.log('Resultado soma com requisicoes:', r_soma_req);
+
+        //Caca tesouro
+        link = exercises['caca-ao-tesouro'].entrada.inicio;
+        let result = await caca_tesouro(link, token);
+        const r_caca = await postResposta('caca-ao-tesouro',result, token);
+        console.log('Resultado caca ao tesouro:', r_caca);
       }
   }
 
@@ -302,5 +308,26 @@ async function soma_com_requisicoes(array, token){
     return s;
 }
 
+async function caca_tesouro(link, token){
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    };
+    let cacando = true;
+    entrada = link;
+    while (cacando){
+        result = await axios.get(entrada,config);
+        // console.log(result);
+        if (Number.isInteger(result.data)) {
+            return result.data;
+        }
+        else{
+            entrada = result.data;
+        }
+    }
+}
 
 main();
